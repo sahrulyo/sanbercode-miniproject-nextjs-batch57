fb96327ca2abc3aea787da113c053415a78573f9
Sahrul Yoyo <111233171+sahrulyo@users.noreply.github.com>
2024-06-30 17:20:46 +0700

    part 1 - navbar

---
+++ sanbers-notes-web-apps-nextjs-batch57/pages/index.js
@@ -0,0 +1,15 @@
+
+import Header from "@/components/header/header ";
+import Footer  from "@/components/footer/footer ";
+
+
+
+export default function Home() {
+  return (
+   <div>
+  
+    <Header/>
+    <Footer/>
+   </div>
+  );
+}
